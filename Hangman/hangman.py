ed103bda34e7e38a8048c6f1f1ed56c7169539b7
Shane Thomas <103258421+shane-thomas@users.noreply.github.com>
2023-01-06 14:10:17 +0530

    Update hangman.py

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,25 +1,26 @@
-#Step 1 
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
 L=[]
-chosen_word=random.choice(word_list)
+chosen_word=str(random.choice(word_list))
 for i in range(len(chosen_word)):
   L+=("_")
 lives=len(chosen_word)
 
 endofgame = False
 while not endofgame:
-  if lives!=0:
+  while lives>0:
     print("\n"*2)
     print(L)
     guess = input("Guess a letter: ").lower()
-    for position in range(len(chosen_word)):
-      letter = chosen_word[position]
-      if guess == letter:
-        L[position] = guess
-      elif guess not in chosen_word:
-        lives-=1
+    if guess not in list(chosen_word):
+          lives=lives-1
+    else:
+      for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if guess == letter:
+          L[position] = guess
+        
 
     if "_" not in L:
       print()
@@ -27,3 +28,4 @@ while not endofgame:
       endofgame=True
   else:
     print("Game over")
+    break
