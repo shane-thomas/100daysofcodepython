f6193c2f2b3f94cbf8fb93f81612a66431328062
Shane Thomas <103258421+shane-thomas@users.noreply.github.com>
2023-01-10 22:03:55 +0530

    Create main.py

---
+++ Calculator/main.py
@@ -0,0 +1,15 @@
+#Add
+def add(n1, n2):
+  return n1+n2
+
+#Subtract
+def subtract(n1, n2):
+  return n1-n2
+
+#Multiply
+def multiply(n1, n2):
+  return n1*n2
+
+def divide(n1, n2):
+  return n1/n2
+
